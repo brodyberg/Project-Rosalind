516309ed63dd81bcfac66b05bd6df498c745b730
brodyberg <brodyberg@gmail.com>
2014-12-07 18:31:09 -0800

    Complete

--- Hamming.fsx
+++ Hamming.fsx
@@ -1,2 +1,4 @@
-Seq.zip "GAGCCTACTAACGGGAT" "CATCGTAATGACGGCCT"
+Seq.zip 
+    "GAGCCTACTAACGGGAT" 
+    "CATCGTAATGACGGCCT"
 |> Seq.fold (fun acc pair -> if not (fst pair = snd pair) then acc + 1 else acc) 0
