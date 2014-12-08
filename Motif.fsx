ee0a74e6184088bb5dd3a3588680d9569efd1319
brodyberg <brodyberg@gmail.com>
2014-12-07 22:16:57 -0800

    Primitive solution

--- Motif.fsx
+++ Motif.fsx
@@ -1,7 +1,6 @@
 //s: GATATATGCATATACTT
 //t: ATAT
 
-
 //output: 2 4 10
 
 // we take s, we take t
@@ -10,31 +9,16 @@
 
 let s = "GATATATGCATATACTT"
 let t = "ATAT"
-//let sub = t.Substring(0, (0 + (t.Length)))
 
 "GATATATGCATATACTT"
 |> Seq.iteri (fun i item -> 
     printfn "i: %d" i
-    match item with
-    | 'A' -> 
-//        if (i < s.Length) && (i + t.Length) < s.Length then
+
+    if item = 'A' then
         if (i < 10) then
-            let sub = s.Substring(i, (i + (t.Length)))
-            printf "sub: %s" sub
+            let sub = s.Substring(i, t.Length)
+            printfn "sub: %s" sub
         
-            if sub = t then printf " %d" i
-    | _ -> printf "_")
-
-
-"GATATATGCATATACTT"
-|> Seq.take 5
-
-
-
-Seq.compareWith
+            if sub = t then printfn "MATCH: %d" i)
 
 // NOTE: answer is 1-based, not zero-based
-
-let findTinS (t:string) (s:string) =
-    s
-    |> Seq.findIndex (fun 
