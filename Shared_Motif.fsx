7a25d61f4c21c491922523b98bd96349cedcf8d0
Brody Berg <brodyberg@gmail.com>
2015-03-05 08:22:34 -0800

    Start shared-motif

---
+++ Shared_Motif.fsx
@@ -0,0 +1,16 @@
+﻿#load "Fasta.fs"
+
+open Rosalind
+
+">Rosalind_1
+GATTACA
+>Rosalind_2
+TAGACCA
+>Rosalind_3
+ATACA"
+|> Fasta.strToFastaSeq
+|> List.ofSeq
+|> (fun lst -> (lst.Head, lst.Tail))
+
+// we need a list, so we can just first and rest
+
