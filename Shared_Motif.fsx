8133300f10250ec4edb42172904f5b68de3c240a
Brody Berg <brodyberg@gmail.com>
2015-03-05 17:27:21 -0800

    Test of extracting & using head and tail

--- Shared_Motif.fsx
+++ Shared_Motif.fsx
@@ -2,6 +2,9 @@
 
 open Rosalind
 
+let printBoth head tail = 
+    printfn "head: %A tail: %A" head tail
+
 ">Rosalind_1
 GATTACA
 >Rosalind_2
@@ -11,6 +14,7 @@ ATACA"
 |> Fasta.strToFastaSeq
 |> List.ofSeq
 |> (fun lst -> (lst.Head, lst.Tail))
+|> (fun (head, tail) -> printBoth head tail)
 
 // we need a list, so we can just first and rest
 
