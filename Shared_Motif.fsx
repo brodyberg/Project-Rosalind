f1883fdb98d3ca041ab928704f0ea0560e796ba7
Brody Berg <brodyberg@gmail.com>
2015-03-09 15:20:07 -0700

    Functions to methods - close testing

--- Shared_Motif.fsx
+++ Shared_Motif.fsx
@@ -1,52 +1,58 @@
 ﻿#load "Fasta.fs"
 #load "FastaCorpora.fs"
 
-let dummySecond = 
-    ">Rosalind_2970
-    GCGAGGTTCGGCATAATAGCAGATGTCGCAAAGGGTAAGTAAACGTTGACGGGGGCGTCC
-    CCTCAGCGTAGTAATATTTACCCTTATAGTCAATCAGACAGACGTATAATTACCGAAGGA
-    CTTAAGACCTCGACATTCGAAATGCAAGCTCGTAGGACCAACCGGCGTCTGTGCTATTTG
-    ATATAGTCTTGTTGCTATAGGGGTCCTGTCCCGACATCCATGGCCCGCTACCCATGCCCA
-    TTTCTCTACGCAGGATGGACTCCGGTCTCATTCCTAAAACGCTACGTACAAATTTCATTA
-    AGCCAGCGCTTAATGAATGCTGTGAAATACGCTAGGGCAGAGTCTGAGGGCAAGGCGGGG
-    GCAGAAGCGCAATGTACGATGACAGGGTGAGCGCCGCGCGGGTATATCATGCTTCAACCA
-    GACCCCCCGATCTCTGCTGTTCGGGCAATCCGCGGCATAATGGTGAGCTACGAGAAATCC
-    CTCCGCATGTTCAACGTCACATCACTGCTCAATCGGTTTTCGCCATAAACCATTGCGTAG
-    TTTAAGGATCACCAGAGGCGACCCATCGAACGTTCGTGGGAGTATCGTTAAAGTACGAAT
-    AGCCCGCAGAGCCCCACGACCGAGGTATCACTCGTACAATCCCCGTGGTTCGGCAAGCTT
-    ACACAATTATAGACTGCGTTCGCCGATATTTTTTTCGAGTGAACAGTAGCCCTAGTTGTC
-    CACGTTACCTGAACCTGGCTGCAACGGCCCTGATCTTCGGCCAGTGGACCCGTAAATATC
-    AGTGAGCCCAGAGCTCGCATAGCTGACTTACAGCGGGTTGGGGGGGGATTGTAAGGGGCC
-    TGAGGCATACAGTTATTGCTGTGTTAGCAACCCCGTCGTATTCCGGAGCATAAAGTGACT
-    CGCGTCGTGTCGTTCATCTTCTGGTATCACCCGGCGCGTATCATAAATACGACGATTCAA
-    ATCAGGAAAAATAAAGACCGCAAATAATGGAGAAGTGGCC"
-
 open Rosalind
 
-let printBoth head tail = 
-    printfn "head: %A tail: %A" head tail
-
-FastaCorpora.exampleLong
-|> Fasta.strToFastaSeq
-|> List.ofSeq
-|> (fun lst -> (lst.Head, lst.Tail))
-
-let (first, rest) = 
-    FastaCorpora.exampleLong
-    |> Fasta.strToFastaSeq
-    |> List.ofSeq
-    |> (fun lst -> (lst.Head, lst.Tail))
-
-let dummyFasta =
-    dummySecond
-    |> Fasta.strToFastaSeq
-    |> Seq.head
+// module Tracker =
 
 type Tracker = 
     { x: int;
       y: int;
       Substrings: Set<string>; 
-      Grid: Map<int * int, int>; }
+      Grid: Map<int * int, int>; } 
+    with
+    member this.MarkMatch () =
+        let grid = this.Grid
+        let originalValue = 
+            let key = (this.x - 1, this.y - 1)
+
+            if grid.ContainsKey(key)
+            then grid.[key]
+            else 0
+
+        let grid' = grid.Add((this.x, this.y), originalValue + 1); 
+            
+        { this with Tracker.Grid = grid'; }
+
+    static member AddSubstring (template:string) tracker = 
+        let value = tracker.Grid.[(tracker.x,tracker.y)]
+        let start = tracker.x - value  + 1
+        let end' = value + start - 1
+                            
+        let theSlice = template.[start..end']
+
+        let x = template.Insert(start, "[")
+        let y = x.Insert(end' + 2, "]")
+
+        //    printfn "(value: %d) slicing %s from %d to %d which is: %s" value right start end' theSlice
+        printfn "(value: %d) slicing: %s which is: %s" value y theSlice
+
+        { tracker with Tracker.Substrings = (tracker.Substrings.Add theSlice) }
+
+    member this.PrintGrid (str:string) =
+        printfn "----------"
+        seq { for x in 0 .. str.Length -> x }
+        |> Seq.iteri
+            (fun x item -> 
+                seq { for y in 0 .. str.Length -> 
+                        let key = (x, y)
+
+                        if this.Grid.ContainsKey key
+                        then this.Grid.[key]
+                        else 0 }
+                |> Seq.iter
+                    (fun item -> printf "%d " item)
+                printfn ">")
 
 let trackerDefault = 
     { Tracker.x = 0;
@@ -54,76 +60,62 @@ let trackerDefault =
       Tracker.Substrings = Set.empty; 
       Tracker.Grid = Map.empty; }
 
-let test1 = "abbcc"
-let test2 = "dbbcc"
-
-let printGrid (grid:Map<(int * int), int>) =
-    printfn "----------"
-    seq { for x in 0 .. 5 -> x }
-    |> Seq.iteri
-        (fun x item -> 
-            seq { for y in 0 .. 5 -> 
-                    let key = (x, y)
-
-                    if grid.ContainsKey key
-                    then grid.[key]
-                    else 0 }
-            |> Seq.iter
-                (fun item -> printf "%d " item)
-            printfn ">")
-
-let updateTracker tracker = 
-    let grid = tracker.Grid
-    let originalValue = 
-        let key = (tracker.x - 1, tracker.y - 1)
-
-        if grid.ContainsKey(key)
-        then grid.[key]
-        else 0
-
-    let grid' = grid.Add((tracker.x, tracker.y), originalValue + 1); 
-            
-    { tracker with Tracker.Grid = grid'; }
-
 let compareTwo left (right:string) =
+    let iteration = ref 0
+
     left
     |> Seq.fold
-        (fun accOuter outerItem ->
-            let interimResult = 
+        (fun outerTracker outerItem ->
+            let interimTracker = 
                 right
                 |> Seq.fold
-                    (fun accInner innerItem ->
+                    (fun innerTracker innerItem ->
+                        innerTracker
                         // optionally mark match in grid
-                        let tracker' = 
-                            if outerItem = innerItem
-                            then updateTracker accInner
-                            else accInner
-
-                        let tracker'' = 
-                            // optionally add substring to tracker set
+                        |> (fun (tracker:Tracker) -> 
+                            if outerItem = innerItem 
+                            then tracker.MarkMatch()
+                            else tracker)
+                        // optionally add substring to tracker set
+                        |> (fun tracker -> 
                             if outerItem = innerItem
                             then 
-                                let value = tracker'.Grid.[(tracker'.x,tracker'.y)]
-                                let start = tracker'.x - value  + 1
-                                let end' = value + start - 1
-                            
-                                let theSlice = right.[start..end']
-
-                                printfn "match: %c %c" outerItem innerItem
-                                printfn "slicing %s from %d to %d which is: %s" right start end' theSlice
-
-                                { tracker' with Tracker.Substrings = (tracker'.Substrings.Add theSlice) }
-                            else
-                                tracker'
-
+                                Tracker.AddSubstring right tracker
+                            else tracker)
                         // walk tracker to next location column
-                        { tracker'' with Tracker.x = tracker''.x + 1; })
-                    { accOuter with Tracker.x = 0; }
+                        |> (fun tracker -> 
+                            let tempTracker = { tracker with Tracker.x = tracker.x + 1; }
+                            tempTracker.PrintGrid right
+
+                            if !iteration % right.Length = 0
+                            then printfn "NEW ROW iteration: %d %c =? %c" !iteration outerItem innerItem
+                            else printfn "iteration: %d %c =? %c" !iteration outerItem innerItem
+                            
+                            iteration := !iteration + 1
+                            tempTracker))
+                    { outerTracker with Tracker.x = 0; }
             // walk tracker to next location row
-            { interimResult with Tracker.y = interimResult.y + 1; })
+            { interimTracker with Tracker.y = interimTracker.y + 1; })
         trackerDefault
-      
+
+let test1 = "abbcc"
+let test2 = "dbbcc"
+// b bb bbc bbcc c cc
+
 let result = compareTwo test1 test2
 
-result.Substrings
-printGrid result.Grid
+let result' = compareTwo "a" "a"
+result'.PrintGrid "a"
+
+let result2 = compareTwo "ab" "xb"
+result2.PrintGrid "ab"
+
+let result'' = compareTwo "xbb" "xbb"
+result''.Substrings.Contains("bb") = true
+result''.PrintGrid "xbb"
+
+let result''' = compareTwo "bbx" "bbx"
+result'''.Substrings.Contains("bb") = true
+result''.PrintGrid "bbx"
+// set ["b"; "bb"; "bbx"];
+
