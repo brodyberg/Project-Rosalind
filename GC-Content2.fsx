7461b9b2fc492d6d1a0cf31307771c2ee24bdaeb
Brody Berg <brodyberg@gmail.com>
2015-03-10 20:22:53 -0700

    Project changes

---
+++ GC-Content2.fsx
@@ -0,0 +1,36 @@
+﻿module Fasta = 
+    type Fasta = 
+        { Name:string; DNA:string; GC_Content:double; }
+        static member print (fasta:Fasta) = printfn "%s" (sprintf "%s\n%f" fasta.Name fasta.GC_Content)
+
+    let strToFastaSeq (str:string) = 
+
+        let split (splitter:char []) (str:string) = str.Split(splitter)
+        let splitOnNewLine (str:string) = split [|'\n'|] str
+        let splitOnChevron (str:string) = split [|'>'|] str
+        let hasContent (str:string) = if str = System.String.Empty then false else true
+            
+        let emptyFasta = { Name = System.String.Empty; 
+                            DNA = System.String.Empty; 
+                            GC_Content = 0.0; }
+        
+        let entryToFasta (lines:string []) =
+            lines
+            |> Seq.fold (fun fasta line -> 
+                match fasta.Name with
+                | "" -> { fasta with Name = line; }
+                | _ -> { fasta with DNA = fasta.DNA + line.Trim(); })
+                emptyFasta
+        
+        str
+        |> splitOnChevron
+        |> Seq.filter hasContent
+        |> Seq.map splitOnNewLine
+        |> Seq.map entryToFasta
+
+">Rosalind_8327
+GTTGAAATTACAATTGTCTCGACGTTCTAAATTAGATTACTTCAGTGTACTGCGCCTATT
+>Rosalind_4695
+GCGAAGGGTTGCAACCATTGTGCTACTACGGTGGCCGAAGCGAATCCATGCCAGTGGAGG"
+|> Fasta.strToFastaSeq
+
